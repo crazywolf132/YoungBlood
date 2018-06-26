e521997761a0cf74749667cf194655914f76e976
crazywolf <crazywolf132@gmail.com>
2018-06-27 01:05:44 +1000

    My current progress.

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 80;
 
+
+/*
+ ██████  ██████  ██████  ███████
+██      ██    ██ ██   ██ ██
+██      ██    ██ ██████  █████
+██      ██    ██ ██   ██ ██
+ ██████  ██████  ██   ██ ███████
+*/
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(
@@ -20,3 +28,130 @@ app.use((req, res, next) => {
     );
     next();
 })
+
+
+/*
+██████   ██████  ██    ██ ████████ ███████ ███████
+██   ██ ██    ██ ██    ██    ██    ██      ██
+██████  ██    ██ ██    ██    ██    █████   ███████
+██   ██ ██    ██ ██    ██    ██    ██           ██
+██   ██  ██████   ██████     ██    ███████ ███████
+*/
+const webhook = require("./routes/webhook");
+app.use("/webhook", webhook);
+
+
+
+
+/*
+███████ ██    ██ ███    ██  ██████ ████████ ██  ██████  ███    ██ ███████
+██      ██    ██ ████   ██ ██         ██    ██ ██    ██ ████   ██ ██
+█████   ██    ██ ██ ██  ██ ██         ██    ██ ██    ██ ██ ██  ██ ███████
+██      ██    ██ ██  ██ ██ ██         ██    ██ ██    ██ ██  ██ ██      ██
+██       ██████  ██   ████  ██████    ██    ██  ██████  ██   ████ ███████
+*/
+
+// The in-memory database...
+
+/**
+ * All information we keep is purely a username, and an age. Along with userID.
+ * We keep this information so we can inform the Listener when you connect. 
+ * As this will make it easier for them.
+ * 
+ * Maybe we could keep a log of the Listeners messages, so then if there is any reports
+ * of miss use of the system, we could see and potentially ban that user from being a listener?
+ * Though, this may break a few of the privacy rules... idk?
+ */
+
+ /**
+  * An example of a connection might be:
+  * {'listenerID':'inneedID', 'inneedID':'listenerID'}
+  * That way we have a solid memory of who is connected to who. 
+  */
+var connections = {}
+
+
+/**
+ * This will be used to remember who is what.
+ * For example, it will remember ur information such as BDAY, USERNAME.
+ * It will also keep information such as whether or not you are a listener
+ */
+var status = {}
+
+
+
+// The exports...
+var share = (module.exports = {});
+
+/**
+ * This function is used for showing the sender that we have recieved their message...
+ * Eventually I would like to make it so the first user only gets this once the second user has seen the message.
+ * @param {String} sender 
+ */
+share.seenMessage = (sender) => {
+    const recipient = _createRecipient(sender);
+    return sendRequest({
+        recipient,
+        sender_action: "mark_seen"
+    });
+};
+
+share.handle = (message, sender) => {
+    /**
+     * We cannot assume that the first person is already connected to the second person...
+     * So we need to perform a few checks.
+     * 1) Check to see if person is in the database... if not, add them.
+     * 2) Check to see what mode they want, Helper or In-need.
+     * Only then can we send the message from person1 to person2.
+     */
+
+    if (sender in connections) {
+        // Cool. so the person is in here... we just have to pass on the message...
+        var partner = connection[sender]; // Collecting their partners ID.
+
+    } else {
+        // We need to ask them for some details now...
+    }
+
+}
+
+
+
+
+
+// Now for the mind numbing functions to make this all possible.
+
+
+sendRequest = (body, endpoint, method) => {
+    endpoint = endpoint || 'messages';
+    method = method || 'POST';
+    return fetch(`https://graph.facebook.com/v2.6/me/${endpoint}?access_token=${token}`, {
+        method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+        .then(res => res.json())
+        .then(res => {
+            return res;
+        })
+        .catch(err => console.log(`Error sending message: ${err}`));
+}
+
+_createRecipient = (recipient) => {
+    return (typeof recipient === 'object') ? recipient : { id: recipient };
+}
+
+
+/*
+██      ██ ███████ ████████ ███████ ███    ██ ███████ ██████
+██      ██ ██         ██    ██      ████   ██ ██      ██   ██
+██      ██ ███████    ██    █████   ██ ██  ██ █████   ██████
+██      ██      ██    ██    ██      ██  ██ ██ ██      ██   ██
+███████ ██ ███████    ██    ███████ ██   ████ ███████ ██   ██
+*/
+setTimeout(() => {
+  app.listen(port);
+  module.exports.logger("NORMAL", `Magic happens on port ${port}\n\n`);
+}, 1000);
