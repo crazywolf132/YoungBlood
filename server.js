af78b7e1d33b47f5b32c497649698cd5e3400d92
crazywolf <crazywolf132@gmail.com>
2018-07-04 01:15:39 +1000

    Fixed all comments, removed all un-needed code

--- server.js
+++ server.js
@@ -52,8 +52,29 @@ const messenger = new YoungBlood({
 ██       ██████  ██   ████  ██████    ██    ██  ██████  ██   ████ ███████
 */
 
+// The in-memory database...
+/**
+ * All information we keep is purely a username, and an age. Along with userID.
+ * We keep this information so we can inform the Listener when you connect.
+ * As this will make it easier for them.
+ *
+ * Maybe we could keep a log of the Listeners messages, so then if there is any reports
+ * of miss use of the system, we could see and potentially ban that user from being a listener?
+ * Though, this may break a few of the privacy rules... idk?
+ */
+
+/**
+ * An example of a connection might be:
+ * {'listenerID':'inneedID', 'inneedID':'listenerID'}
+ * That way we have a solid memory of who is connected to who.
+ */
 var interactions = {};
 
+/**
+ * This will be used to remember who is what.
+ * For example, it will remember ur information such as BDAY, USERNAME.
+ * It will also keep information such as whether or not you are a listener
+ */
 var information = {};
 
 var waitingListeners = [];
@@ -63,7 +84,7 @@ var waitingInneeds = [];
 /**
  *	To make it easier on the user to distinguish what messages are from a human,
  *	and what are from the bot, we will use some emojies...
- h */
+ */
 
 const connect2people = (person1, person2) => {
 	information[person1].waiting = false;
@@ -348,97 +369,6 @@ messenger.on("message", (payload, chat) => {
 	}
 });
 
-messenger.start(); /*
-
-
-
-// The in-memory database...
-// The exports...
-var share = (module.exports = {});
-/**
- * All information we keep is purely a username, and an age. Along with userID.
- * We keep this information so we can inform the Listener when you connect.
- * As this will make it easier for them.
- *
- * Maybe we could keep a log of the Listeners messages, so then if there is any reports
- * of miss use of the system, we could see and potentially ban that user from being a listener?
- * Though, this may break a few of the privacy rules... idk?
- */
-
-/**
- * An example of a connection might be:
- * {'listenerID':'inneedID', 'inneedID':'listenerID'}
- * That way we have a solid memory of who is connected to who.
- */
-//var connections = {}
-
-/**
- * This will be used to remember who is what.
- * For example, it will remember ur information such as BDAY, USERNAME.
- * It will also keep information such as whether or not you are a listener
- */
-//share.db = status = {};
-
-/**
- * This function is used for showing the sender that we have recieved their message...
- * Eventually I would like to make it so the first user only gets this once the second user has seen the message.
- * @param {String} sender
- */
-/*share.seenMessage = (sender) => {
-    const recipient = _createRecipient(sender);
-    return sendRequest({
-        recipient,
-        sender_action: "mark_seen"
-    });
-};*/
-
-// Loading this here so then it gets access to the above exports...
-
-/**
- * This is where every message is processed. Everything that comes through the bot,
- * will first come here... so we can workout what the hell to do with it.
- * @param {String} message
- * @param {String} sender
- */
-/*share.handle = (message, sender) => {
-    /**
-     * We cannot assume that the first person is already connected to the second person...
-     * So we need to perform a few checks.
-     * 1) Check to see if person is in the database... if not, add them.
-     * 2) Check to see what mode they want, Helper or In-need.
-     * Only then can we send the message from person1 to person2.
-     */
-/*console.log(`Recieved from ${sender}: ${message}`);
-
-    if (sender in connections && status[sender].gettingStartedStage == -1) {
-        // Cool. so the person is in here... we just have to pass on the message...
-        var partner = connection[sender]; // Collecting their partners ID.
-
-    } else {
-        // We need to ask them for some details now...
-        // First we are going to check to see if we have ever seen this person...
-        if (sender in status && status[sender].gettingStartedStage == -1) {
-            // Guess we dont need to ask them any questions... We already have their information.
-            // We just need to connect them to someone.
-            // or check to see if they have entered a known command.
-
-        } else {
-            // This person is brand new. Lets get them setup.
-            // We will send them a few messages asking for some details, aswell as saying what this service is.
-            // Aswell as what information we will be keeping and such.
-            status[sender] = {}
-            status[sender].username = ""
-            status[sender].gettingStartedStage = 0;
-            presets.getStarted(messenger.respond, sender);
-        }
-    }
-*/
-
-/*messenger.on('message', (payload, chat) => {
-    const text = payload.message.text;
-    chat.say(`Echo: ${text}`);
-});*/
-
 /*
 ██      ██ ███████ ████████ ███████ ███    ██ ███████ ██████
 ██      ██ ██         ██    ██      ████   ██ ██      ██   ██
@@ -446,4 +376,5 @@ var share = (module.exports = {});
 ██      ██      ██    ██    ██      ██  ██ ██ ██      ██   ██
 ███████ ██ ███████    ██    ███████ ██   ████ ███████ ██   ██
 */
-//messenger.start();
+
+messenger.start();
